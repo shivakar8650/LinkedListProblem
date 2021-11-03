4e8d4b82a7a897c8ee29390759068829d16fc544
shivakar <shivakar@ch.iitr.ac.in>
2021-11-03 23:35:26 +0530

    [shivakar] ADD:UC1 is added

---
+++ LinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListProblem
+{
+    public class LinkedList
+    {
+        internal Node head;
+
+        internal void  add(int data)
+        {
+            Node node = new Node(data);
+            if(head == null)
+            {
+               head = node;
+            }
+            else
+            { Node temp = head;
+                  while(temp.next != null)
+                {
+                   temp = temp.next;
+                }
+                temp.next = node;
+            }
+            
+            Console.WriteLine("added :" + node.data);
+        }
+
+        internal void display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Linked is empty");
+
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    Console.WriteLine(temp.data + " ");
+                    temp = temp.next;
+                }
+                Console.WriteLine(temp.data + " ");
+
+            }
+        }
+
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LinkedListProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            LinkedList linkedlist = new LinkedList();
+            linkedlist.add(56);
+            linkedlist.add(30);
+            linkedlist.add(70);
+          
+            linkedlist.display();
+         
+
+        }
+    }
+}
