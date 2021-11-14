b5c0aa6069ffa8b5e56a68b16550cc7e70c188d0
shivakar <shivakar@ch.iitr.ac.in>
2021-11-14 16:11:12 +0530

    [shivakar]added: sorted linked list

--- LinkedList.cs
+++ LinkedList.cs
@@ -16,52 +16,41 @@ namespace LinkedListProblem
                head = node;
             }
             else
-            { Node temp = head;
-              Node prev = null;
-                  while(temp.next != null)
+            {
+                Node temp = head;
+                Node prev = temp;
+                int checklast = 0;
+                if (temp.next == null)
                 {
-                  if ((temp.data).CompareTo(data) > 0)
+                    if ((temp.data).CompareTo(data) > 0)
+                    head =addFirst(temp, node);
+                    else
+                        addLast(temp, node);
+                    checklast = 1;
+                }
+                while (temp.next != null)
+                {
+                    if ((temp.data).CompareTo(data) > 0)
                     {
-                        Node tempNode = prev.next;
-                        prev.next = node;
-                        prev.next.next = tempNode;
-                        break;
+                        InsertAfter(prev.data, data);
+                        checklast = 1;
                     }
                     prev = temp;
                     temp = temp.next;
-                }
-                if ((temp.data).CompareTo(data) > 0 && temp.next==null && prev != null)
-                {
-                    node.next = temp;
-                   
-                        prev.next = node;
-                }
-                else if ((temp.data).CompareTo(data) > 0 && temp.next == null && prev == null)
-                {
-                    node.next = head;
-                    head.next = node;
-                   
-                }
-                else
-                {
-                    temp.next = node;
-                }
 
+                }
+                if (checklast == 0)
+                {
+                    if ((temp.data).CompareTo(data) > 0)
+                        InsertAfter(prev.data, data);
+                    else
+                        addLast(temp, node);
+                }
             }
-            
             Console.WriteLine("added :" + node.data);
+        
         }
-/*
-        Node<T> searchedNode = head;
-                    //Contain Data of previous Node
-                    //while ( Comparer<T>.Default.Compare(temp.data,data) !=0 )
-                    while (!searchedNode.data.Equals(specifiedNodeData))
-                    {
-                        searchedNode = searchedNode.next;
-                    }
-    Node<T> tempNode = searchedNode.next;
-    searchedNode.next = node;
-                    searchedNode.next.next = tempNode;*/
+
         internal void display()
         {
             if (head == null)
@@ -81,29 +70,95 @@ namespace LinkedListProblem
                 Console.WriteLine(" ");
             }
         }
-
-      
-      
-        internal void Find(int data)
+        internal Node addFirst(Node temp, Node node)
         {
-            if(head == null)
+            node.next = temp;
+            return node;
+        }
+
+        internal void addLast(Node temp, Node node)
+        {
+            temp.next = node;
+        }
+      
+        internal void InsertAfter(int specifiedNodeData, int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
             {
-                Console.WriteLine("List is empty");
+                head = node;
             }
             else
             {
-                Node temp = head;
-                while(!temp.data.Equals(data) && temp.next != null)
+                Node searchedNode = head;
+                while (!searchedNode.data.Equals(specifiedNodeData))
                 {
-                    temp = temp.next;
+                    searchedNode = searchedNode.next;
                 }
-                if (!temp.data.Equals(data) && temp.next == null)
-                    Console.WriteLine("element not found");
-                else
-                    Console.WriteLine("element is found");
-
-                        
+                Node tempNode = searchedNode.next;
+                searchedNode.next = node;
+                searchedNode.next.next = tempNode;
             }
         }
+       
+
+
+        }
     }
+
+
+
+
+
+/*
+Node temp = head;
+Node prev = temp;
+int checklast = 0;
+while (temp != null)
+{
+    if ((temp.data).CompareTo(data) > 0 && temp.next == null)
+    {
+        head = addFirst(temp, node);
+        checklast = 1;
+    }
+    else if ((temp.data).CompareTo(data) > 0)
+    {
+        InsertAfter(prev.data, data);
+        checklast = 1;
+    }
+    prev = temp;
+    temp = temp.next;
+
 }
+if (checklast == 0)
+{
+    addLast(prev, node);
+}*/
+
+/*public void Sort()
+{
+    Node<T> prev = this.head, curr = null;
+    T temp;
+    if (this.head == null)
+    {
+        return;
+    }
+    else
+    {
+        while (prev != null)
+        {
+            curr = prev.Next;
+            while (curr != null)
+            {
+                if (prev.data.CompareTo(curr.data) > 0)
+                {
+                    temp = prev.data;
+                    prev.data = curr.data;
+                    curr.data = temp;
+                }
+                curr = curr.Next;
+            }
+            prev = prev.Next;
+        }
+    }
+}*/
